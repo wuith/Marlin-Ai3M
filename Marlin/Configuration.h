d244506447ae65227eb06f9c47e2c73995bd4e22
wuith <56004029+wuith@users.noreply.github.com>
2020-04-25 17:51:56 +0200

    Bed size 220x220
    Bed size 220x220

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -896,11 +896,11 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 215
-#define Y_BED_SIZE 215
+#define X_BED_SIZE 220
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -5
+#define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
